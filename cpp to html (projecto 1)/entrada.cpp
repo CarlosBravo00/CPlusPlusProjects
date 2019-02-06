944b083e51b54709b7fad2115840596e435ad75d
Carlos <carlosemiliobravomarquez@gmail.com>
2019-02-06 17:39:46 -0600

    Laptop
    Archivos mas actualizados

--- cpp to html (projecto 1)/entrada.cpp
+++ cpp to html (projecto 1)/entrada.cpp
@@ -1,51 +1,50 @@
 /**
-@progName Act3
-@desc Programa que contiene varios ejercisios de Strings
-@author Carlos Bravo
-@date 24/1/2019
+@progName Factorial
+@desc Programa que tiene una función para calcular el factorial de un número
+@author Juan Perez
+@date 28 de Enero de 2019
 */
-#include <iostream>
-#include <string>
 
-using namespace std;
+#include <iostream> using namespace std;
 
 /**
-@funcName Ej 1
-@desc programa que pide una frase y que muestre un mensaje que indica si se trata de un palíndromo o no
+@funcName factorial
+@desc Calcula el factorial de un número
+@param iNum valor para el que se va a calcular el factorial. Debe ser entero positivo.
+@return factorial del valor iNum
 */
-void ej1(){
-    cout<<"Ejercicio 1"<<endl;
-    string palabra;
-    cout<<"Teclear una frase: "<<endl<<endl;
-    getline(cin,palabra);
-    string fraseoriginal=palabra;
+int factorial(int iNum)
+{
+int iFac=1;
 
-   //borrar espacio
-    int espacio=0;
-    while (espacio>=0){
-        espacio=palabra.find(" ");
-        if(espacio>=0){
-            palabra.erase(espacio,1);
-        }
-    }
-
-
-    //validacion
-    bool palindromo= true;
-    int longitudfrase = palabra.size();
-    int j=longitudfrase-1;
-    for(int i=0;i<longitudfrase/2 && palindromo; i++){
-        if(tolower (palabra[i])!= tolower (palabra[j])){
-            palindromo=false;
-        }
-        else{
-            j--;
-        }
-    }
-    if(palindromo){
-            cout<<endl<<"la frase "<<fraseoriginal<<" si es un palindromo"<<endl;
-    }else{
-        cout<<endl<<"la frase "<<fraseoriginal<<" no es un palindromo"<<endl;
-
-    }
+// Calculate the factorial with a FOR loop for(int iC=1; iC<=iNum; iC++)
+{
+iFac = iFac*iC;
 }
+
+return iFac; // This value is returned to caller
+}
+
+/**
+@funcName validaEntero
+@desc Función que pide al usuario un valor y valida que sea entre 1 y 10
+@return regresa el valor entero que tecleó el usuario (entre 1 y 10)
+*/
+int validaEntero()
+{
+int iNum; do
+{
+cout << "Numero "; cin >> iNum;
+} while (iNum < 1 || iNum > 10); return iNum;
+}
+
+/**
+@funcName main
+@desc Pide el valor al usuario, calcula el factorial y lo muestra
+*/
+int main()
+{
+int iNumero, iResult; iNumero = validaEntero(); iResult = factorial(iNumero);
+cout << "Factorial = " << iResult << endl; return 0;
+}
+

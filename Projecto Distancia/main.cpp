cb111fdbf5c39bffc38f462aadcf35b7bbbd0d1d
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2019-02-28 21:18:21 -0600

    idk

--- Projecto Distancia/main.cpp
+++ Projecto Distancia/main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main()
 {
-    Distancia a;
+
     Distancia b(5,11);
     Distancia c(0,4);
 
     Distancia d= b.sumaDistancia(c);
 
-    cout<<"Pies: "<<d.getPies()<<" Pulgadas: "<<d.getPulgadas();
+    cout<<"Pies: "<<d.getPies()<<" Pulgadas: "<<d.getPulgadas()<<" Son: "<<d.convierteAMts()<<" Metros";
     return 0;
 }

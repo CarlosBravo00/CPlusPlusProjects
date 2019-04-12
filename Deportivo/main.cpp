6ea2159ba932c1255cca5158d62e1a9d943d508f
Carlos <carlosemiliobravomarquez@gmail.com>
2019-04-12 11:41:27 -0500

    Revert "NADA"
    This reverts commit 71dc056372f98c02bbf2eb0d36463883edf372c5.

--- Deportivo/main.cpp
+++ Deportivo/main.cpp
@@ -9,9 +9,11 @@ using namespace std;
 
 int main()
 {
-    Servicio *Servicios[20];
-    Reserva Reservaciones[50];
-
+    Reloj r1;
+    Reserva r2;
+    Servicio *r3;
+    Cancha r4;
+    Maquina r5;
 
 
     return 0;
